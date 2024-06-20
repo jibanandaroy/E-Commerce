6d3ac0d3b1d7d60a1f074999c59773947b06f472
DANIGAIN <danigain1234@gmail.com>
2024-06-20 11:49:50 +0600

    order table create

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,20 +1,20 @@
 const Order = require('../models/order');
-const User = require('../models/user');
 
 const placeOrder = async (req,res) =>{
+    const{userId,items,amount,address} = req.body
     try{
-        const newOrder = new Order({
-            userId:req.body.userId,
-            items:req.body.items,
-            amount:req.body.amount,
-            address:req.body.address,
-        })
-        await newOrder.save()
+        const obj = {
+            userId,
+            items,
+            amount,
+            address
+        }
+         const order = await Order.create(obj);
         res.json({success:true,message:"Order Successful"})
     }catch(error){
         console.log(error);
         res.json({success:false,message:"Error"})
-    }
+    } 
 }
 
 module.exports = {placeOrder}
